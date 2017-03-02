0f12a4796074608268115867c7cb64e70bf452b9
Benjamin de Rochemont and Micky Matthews <benjamin@>
2017-03-01 19:18:22 -0800

    add form and js to create list based on user input

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function(){
+  var groceryList = [];
+  $("button#add-item").click(function(){
+    var foodItem = $("#input-field").val().toUpperCase();
+    groceryList.push(foodItem);
+    $("#input-field").val('');
+    console.log(groceryList);
+  });
+  $("button#generate").click(function(){
+    groceryList.sort();
+    groceryList.forEach(function(listItem){
+      $("#grocery-list").append("<li>" + listItem + "</li>");
+    });
+  });
+
+});
